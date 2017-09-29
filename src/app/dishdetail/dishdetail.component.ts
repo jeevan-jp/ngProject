018387259569f72206065381ca5f9e49e3157942
jeevan-jp <jiwanprakash65@gmail.com>
2017-09-30 00:54:17 +0530

    RxJS part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,8 @@ import { Location } from '@angular/common'; //Allows to track location of page i
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -16,15 +18,27 @@ import { DishService } from '../services/dish.service';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
   //Changes into constructor are made to get hold of dish :id by using various services
   constructor(private dishservice: DishService,
     private route: ActivatedRoute, 
     private location: Location) { }
 
-  ngOnInit() {
-    let id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id).subscribe( dish => this.dish = dish );
-  }
+    ngOnInit() {
+      
+          this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+          this.route.params
+            .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+            .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+        }
+      
+        setPrevNext(dishId: number) {
+          let index = this.dishIds.indexOf(dishId);
+          this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+          this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
+        }
 
   goBack(): void {
     this.location.back();
