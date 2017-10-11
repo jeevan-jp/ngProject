3190bd7a5797c8f0ac3a14e906e08632c73eb7bc
jeevan-jp <jiwanprakash65@gmail.com>
2017-10-11 20:57:47 +0530

    REST errorFree

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -23,7 +23,7 @@ export class DishService {
   }
 
   getDish(id: number): Observable<Dish> {
-    return this.restangular.one('dishees', id).get();
+    return this.restangular.one('dishes', id).get();
   }
 
   getFeaturedDish(): Observable<Dish> {
